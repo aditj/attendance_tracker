4a6d83ae759044ce9bdfcb0682add3a34e85be27
aditj <aditjain1980@gmail.com>
2019-08-11 13:33:26 +0530

    added csrf exempt 1

--- attendance/input/views.py
+++ attendance/input/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect,HttpResponse
+from django.shortcuts import render,redirect,HttpResponse,csrf_exempt
 from .models import Attendance
 
 # Create your views here.
